7977cbbde6f3430a1b4dae7d1654b1bb1da8bc96
Charlesbibi <93184338+Charlesbibi@users.noreply.github.com>
2022-01-18 14:01:52 +0800

    Update BeginWindow.xaml.cs

--- BeginWindow.xaml.cs
+++ BeginWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace GUIXiangQiGame
         public BeginWindow()
         {
             InitializeComponent();
-            ShowGrid(true);//一开始展示棋盘
+            ShowGrid(true);//一开始展示棋盘  Show the chessboard at the beginning
             ChangeState(GAMESTATE.SelectPieces);//转换选子
         }
         //三种游戏状态
@@ -32,9 +32,9 @@ namespace GUIXiangQiGame
         {
             SelectMove, SelectPieces, Win
         }
-        //返回对应棋子图片的文件名以供Uri添加背景图片
+        //返回对应棋子图片的文件名以供Uri添加背景图片  Returns the file name of the corresponding chess piece picture for URI to add a background picture
         private static String SetChessUri(Button target)
-        {   //目标棋子的DataContext绑定了对应的棋子
+        {   //The DataContext of the target chess piece is bound to the corresponding chess piece
             Chess targetChess = (Chess)target.DataContext;
             //三目运算符以获取当前棋子的颜色 与Uri的Black或者Red对应
             String color = (targetChess.Colour == Chess.ChessColour.RED ? "Red" : "Black");
@@ -63,8 +63,8 @@ namespace GUIXiangQiGame
         private void ShowGrid(bool CanClick)
         {
             Button mybutton;
-            int row = XiangQiGrid.RowDefinitions.Count;//xaml中定义的Grid行数
-            int col = XiangQiGrid.ColumnDefinitions.Count;//xaml中定义的Grid列数
+            int row = XiangQiGrid.RowDefinitions.Count;//xaml中定义的Grid行数  row number
+            int col = XiangQiGrid.ColumnDefinitions.Count;//xaml中定义的Grid列数  column number
 
             for (int i = 0; i < row; i++)
             {
@@ -75,7 +75,7 @@ namespace GUIXiangQiGame
                     //设置透明的按钮以展示Grid背景
                     mybutton.Background = Brushes.Transparent;
                     mybutton.BorderBrush = Brushes.Transparent;
-                    XiangQiGrid.Children.Add(mybutton);//button与在xaml中定义的 Grid 的对应位置绑定
+                    XiangQiGrid.Children.Add(mybutton);//button与在xaml中定义的 Grid 的对应位置绑定  binding button
                     mybutton.DataContext = table.chessboard[i, j];//与当前i，j对应的chessboard上棋子绑定
                     if (mybutton.DataContext != null)//chessboard上没有的棋子则不考虑添加图片
                     {   
@@ -85,14 +85,15 @@ namespace GUIXiangQiGame
                         );
                     }
                     if (CanClick)
-                        mybutton.Click += Play_Button_Click;//添加点击事件
+                        mybutton.Click += Play_Button_Click;//添加点击事件 click event
                    
                     mybutton.SetValue(Grid.RowProperty, i);//设置button所在的行
                     mybutton.SetValue(Grid.ColumnProperty, j);//设置button所在的列
                 }
             }
         }
-        //重载方法，用于选完子后展示可以走的路径 return isCanMove to avoid some chess pieces which can't move successfully after select 
+        //重载方法，用于选完子后展示可以走的路径  show the feasible path
+        //return bool to avoid some chess pieces which can't move successfully after select 
         private bool ShowGrid(int NowRow,int NowCol)
         {
             Button mybutton;
@@ -108,7 +109,7 @@ namespace GUIXiangQiGame
                     //设置通明的按钮以展示Grid背景
                     mybutton.Background = Brushes.Transparent;
                     mybutton.BorderBrush = Brushes.Transparent;
-                    //模拟路径,这里只用到canSuccessMove
+                    //Analog path,Here only use args canSuccessMove
                     (bool CanSuccessMove,bool isWin) = table.MovingChess(false,NowCol + 1, NowRow + 1, j + 1, i + 1);
                     
                     XiangQiGrid.Children.Add(mybutton);
@@ -122,20 +123,20 @@ namespace GUIXiangQiGame
                     }
                     mybutton.Click += Play_Button_Click;
 
-                    if (CanSuccessMove) { //标出可走路径
-                        mybutton.BorderBrush = Brushes.Green;//绿色边框
-                        mybutton.BorderThickness = new Thickness(7,7,7,7);//突出button边距厚度
+                    if (CanSuccessMove) { //Mark the walkable path
+                        mybutton.BorderBrush = Brushes.Green;//Green border
+                        mybutton.BorderThickness = new Thickness(7,7,7,7);//Highlight button margin thickness
                         isCanMove = true;
                     }
                     
-                    if(i == NowRow && j == NowCol)//用鼠标右击事件绑定当前选中的棋子
+                    if(i == NowRow && j == NowCol)//Right click the event to bind the currently selected piece
                         mybutton.MouseRightButtonDown += RightButtonDown;
                     //每一个按钮都与Grid中对应的一个网格绑定
                     mybutton.SetValue(Grid.RowProperty, i);//设置button所在的行
                     mybutton.SetValue(Grid.ColumnProperty, j);//设置button所在的列
                 }
             }
-            return isCanMove;//返回值用于判断当前棋子是否有可走的路径
+            return isCanMove;//The return value is used to judge whether the current chess piece has a walkable path
         }
         //当点击右键时，视为取消当前棋子的下棋状态，重新回到SelectPieces
         private void RightButtonDown(object sender, MouseButtonEventArgs e)
@@ -145,14 +146,14 @@ namespace GUIXiangQiGame
             ShowGrid(true);
         }
 
-        static int NowRow;//当前行
-        static int NowCol;//当前列
-        static int DesRow;//目的地行
-        static int DesCol;//目的地列
-        static Boolean isRed = true;//判断当前是不是红色走
-        static int ScoreBlack = 0;//黑色的分数
-        static int ScoreRed = 0;//红色的分数
-        //每次点击Grid 中的Button 时判断当前状态是下子还是选子
+        static int NowRow;  //Current row
+        static int NowCol;  //Current column
+        static int DesRow;  //Destination row
+        static int DesCol;  //Destination column
+        static Boolean isRed = true;  //Judge whether the current is red
+        static int ScoreBlack = 0;  //score of black
+        static int ScoreRed = 0;  //score of red
+        //Each time you click the button in the grid, judge whether the current status is sub or sub
         public void Play_Button_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -161,40 +162,40 @@ namespace GUIXiangQiGame
 
                 switch (gameState)
                 {
-                    case GAMESTATE.SelectMove://下子
-                        DesRow = (int)button.GetValue(Grid.RowProperty);//通过在Grid的行获取在chessboard上对应的行
-                        DesCol = (int)button.GetValue(Grid.ColumnProperty);//通过在Grid的列获取在chessboard上对应的列
+                    case GAMESTATE.SelectMove:  //下子
+                        DesRow = (int)button.GetValue(Grid.RowProperty);  //通过在Grid的行获取在chessboard上对应的行
+                        DesCol = (int)button.GetValue(Grid.ColumnProperty);  //通过在Grid的列获取在chessboard上对应的列
 
                         bool isSuccessMove;
                         bool isWin;
                         (isSuccessMove, isWin) = table.MovingChess(true,NowCol + 1, NowRow + 1, DesCol + 1, DesRow + 1);//真正走子
 
-                        if (!isWin && isSuccessMove)//下子成功但是没赢
+                        if (!isWin && isSuccessMove)//move succeeded but didn't win
                         {
-                            XiangQiGrid.Children.Clear();//清除原先的记录
+                            XiangQiGrid.Children.Clear();//Clear original record
                             ShowGrid(true);
                             ChangeState(GAMESTATE.SelectPieces);
-                            isRed = !isRed;//转换对手
+                            isRed = !isRed;//Convert opponent
                         }
                         else if (isWin)//赢了
                         {
                             ChangeState(GAMESTATE.Win);
-                            XiangQiGrid.Children.Clear();//清除原先的记录
-                            ShowGrid(false);//赢了之后则不可以再响应点击
+                            XiangQiGrid.Children.Clear();//Clear original record
+                            ShowGrid(false);  //赢了之后则不可以再响应点击
 
                             Chess.ChessColour colour = ((Chess)button.DataContext).Colour;//获取棋子颜色
                             if (colour == Chess.ChessColour.RED) {
                                 MessageBox.Show("Black side is Win !", "Congratulate");//后面为标题
-                                ScoreBlack++;//黑色分数加一
-                                BlackScore.Text = "Black Side  :  " + ScoreBlack;//同步展示到黑色的分数框
+                                ScoreBlack++;//Black score plus one
+                                BlackScore.Text = "Black Side  :  " + ScoreBlack;  //Synchronized display to black score box
                             } else {
                                 MessageBox.Show("Red side is Win !", "Congratulate");
-                                ScoreRed++;//红色分数加一
-                                RedScore.Text = "Red Side  :  " + ScoreRed;//同步展示到红色的分数框
+                                ScoreRed++;//Red score plus one
+                                RedScore.Text = "Red Side  :  " + ScoreRed;//Synchronously displayed in the red score box
                             }
                             return;
                         }
-                        else{//选择的点不能下，则重新展示路径，重新选地方下棋
+                        else{//If the selected point cannot be played, the path will be displayed again and the place will be selected again to play chess
                             XiangQiGrid.Children.Clear();//清除原先的记录
                             ShowGrid(NowRow, NowCol);//展示可行路径
                         }
@@ -204,7 +205,7 @@ namespace GUIXiangQiGame
                         NowRow = (int)button.GetValue(Grid.RowProperty);
                         NowCol = (int)button.GetValue(Grid.ColumnProperty);
                        
-                        //如果点击的坐标合法则开始下子阶段
+                        //If the clicked coordinates are legal, start the sub phase
                         if (table.SelectingChess(NowCol + 1, NowRow + 1, isRed))
                         {
                             XiangQiGrid.Children.Clear();//清除原先的记录
@@ -219,10 +220,10 @@ namespace GUIXiangQiGame
                         break;
                 }
             }
-            catch (Exception ex)//catch 异常
+            catch (Exception ex)//catch exception if program have
             {
                 MessageBox.Show(ex.Message,"Error");//show error message
-                ChangeState(GAMESTATE.SelectPieces);//有异常出现则重新选棋
+                ChangeState(GAMESTATE.SelectPieces);//If any abnormality occurs, re select chess
             }
         }
         
@@ -230,10 +231,10 @@ namespace GUIXiangQiGame
         {   
             Button button = (Button)sender;
             //重置
-            if (button.Name == "Reset") {   //一个提示窗口，如果选择OK 则重置，No 则取消
+            if (button.Name == "Reset") {   //A prompt window. If you select OK it will reset or select no it will continue
                 if (MessageBox.Show("Reset NOW ?", "Confirm Message",MessageBoxButton.OKCancel) == MessageBoxResult.OK)   {
                     isRed = true;
-                    table = new Table();//新的table,对应新的棋子
+                    table = new Table();//A new table corresponds to a new piece
                     ChangeState(GAMESTATE.SelectPieces);
                     XiangQiGrid.Children.Clear();//清除原先的记录
                     ShowGrid(true);
@@ -251,11 +252,11 @@ namespace GUIXiangQiGame
 
         private void ChangeState(GAMESTATE state) {
             gameState = state;
-            //将当前状态同步到textbolck,方便玩家下棋
+            //Synchronize the current status to textpolck to facilitate players to play chess
             switch (gameState)
             {
                 case GAMESTATE.SelectPieces:
-                    GameState.Text = "Select Pieces";
+                    GameState.Text = "Select Pieces";//GameState is the element define in .xaml
                     break;
                 case GAMESTATE.SelectMove:
                     GameState.Text = "Select Move";
